bdff125b40e1c498e2d5578020e17d51a51f98de
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2015-03-11 10:10:25 +0700

    Main.class
    iki main pak

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+	public static void main (String[] args){
+		System.out.println("Achmad Fauzi Makharim");
+		System.out.println("08 Desember 1995");
+		System.out.println("Politekhnik Negeri Malang"); 
+		System.out.println("www.Bonyokpunk.com"); 
+	}
+}
